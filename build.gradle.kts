676eeb7f8c2516155e0b06aa7d0ecbdd3acd743d
Joffrey Bion <joffrey.bion@gmail.com>
2021-03-21 01:10:58 +0100

    Complete rework of build/CI/CD

---
+++ build.gradle.kts
@@ -0,0 +1,119 @@
+plugins {
+    `java-library`
+    jacoco
+    checkstyle
+    `maven-publish`
+    signing
+    id("io.github.gradle-nexus.publish-plugin") version "1.0.0"
+    id("org.hildan.github.changelog") version "1.6.0"
+}
+
+group = "org.hildan.generics"
+description = "A way to recursively build something out of a generic type declaration"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+repositories {
+    mavenCentral()
+}
+
+configurations {
+    register("checkstyleConfig")
+}
+
+dependencies {
+    compileOnly("org.jetbrains:annotations:15.0")
+
+    testImplementation("junit:junit:4.+")
+    testImplementation("com.google.code.gson:gson:2.8.2")
+
+    "checkstyleConfig"("org.hildan.checkstyle:checkstyle-config:2.2.0")
+}
+
+checkstyle {
+    maxWarnings = 0
+    toolVersion = "8.8"
+    config = resources.text.fromArchiveEntry(configurations["checkstyleConfig"], "checkstyle.xml")
+}
+
+jacoco {
+    toolVersion = "0.8.1"
+}
+
+tasks.jacocoTestCoverageVerification {
+    violationRules {
+        rule {
+            limit {
+                minimum = 1.0.toBigDecimal()
+            }
+        }
+    }
+}
+tasks.check.get().dependsOn(tasks.jacocoTestCoverageVerification)
+
+val sourcesJar by tasks.creating(Jar::class) {
+    archiveClassifier.set("sources")
+    from(sourceSets.main.get().allSource)
+}
+
+val javadocJar by tasks.creating(Jar::class) {
+    group = JavaBasePlugin.DOCUMENTATION_GROUP
+    archiveClassifier.set("javadoc")
+    from(tasks.javadoc)
+}
+
+nexusPublishing {
+    packageGroup.set("org.hildan")
+    repositories {
+        sonatype()
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+
+            artifact(sourcesJar)
+            artifact(javadocJar)
+
+            val githubUser = findProperty("githubUser") as String? ?: System.getenv("GITHUB_USER")
+            val githubSlug = "$githubUser/${rootProject.name}"
+            val githubRepoUrl = "https://github.com/$githubSlug"
+
+            pom {
+                name.set(project.name)
+                description.set(project.description)
+                url.set(githubRepoUrl)
+                licenses {
+                    license {
+                        name.set("The MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("joffrey-bion")
+                        name.set("Joffrey Bion")
+                        email.set("joffrey.bion@gmail.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:$githubRepoUrl.git")
+                    developerConnection.set("scm:git:git@github.com:$githubSlug.git")
+                    url.set(githubRepoUrl)
+                }
+            }
+        }
+    }
+}
+
+signing {
+    val signingKey: String? by project
+    val signingPassword: String? by project
+    useInMemoryPgpKeys(signingKey, signingPassword)
+    sign(publishing.publications["maven"])
+}
